00387f471ee1c4237f7b2966bc8839e5d13df55f
jjos425 <jjos425@aucklanduni.ac.nz>
2022-08-09 22:36:12 +1200

    Now starting data science part instead of front end

--- FRONTEND/my-app/src/App.tsx
+++ FRONTEND/my-app/src/App.tsx
@@ -1,18 +1,24 @@
-import { useState } from 'react';
+import { useState} from 'react';
 import axios from "axios";
 import './App.css';
-
+import { ChakraProvider,Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon, Box } from '@chakra-ui/react'
+// fetch and 
 function App() {
   const [characterName, setCharacterName] = useState("");
   const [characterInfo, setCharacterInfo] = useState<undefined | any>(undefined);
   const GENSHIN_BASE_API_URL = "https://api.genshin.dev";
   const [currentCharacterName, setCurrentCharacterName] = useState("");
   return (
+    <ChakraProvider>
     <div>
-      <h1>Character Search</h1>
+      <h1>Genshin Character Search</h1>
 
       <div>
-        <label>Pokemon Name</label>
+        <label>Character Name</label>
         <br />
         <input
           type="text"
@@ -27,19 +33,53 @@ function App() {
       <p>You have entered {currentCharacterName}</p>
 
       {characterInfo === undefined ? (
-        <p>Character not found</p>
+        <p style={{fontSize:20}}>Character not found</p>
       ) : (
-        <div id="pokemon-result">
-          <img src={GENSHIN_BASE_API_URL + "/characters/" + currentCharacterName + "/card"} />
+        <div id="character-result">
+          <img width="280" height="auto"src={GENSHIN_BASE_API_URL + "/characters/" + currentCharacterName + "/card"} />
+        <Accordion allowMultiple>
+          <AccordionItem>
+            <h2>
+              <AccordionButton>
+                <Box flex='1' textAlign='left'>
+                  Best Artifacts
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel pb={4}>
+            </AccordionPanel>
+          </AccordionItem>
+
+          <AccordionItem>
+            <h2>
+              <AccordionButton>
+                <Box flex='1' textAlign='left'>
+                  Teams
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel pb={4}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+              veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+              commodo consequat.
+            </AccordionPanel>
+          </AccordionItem>
+      </Accordion>
         </div>
       )}
     </div>
+    
+    </ChakraProvider>
   );
 
   function search() {
     setCurrentCharacterName(characterName);
     axios.get(GENSHIN_BASE_API_URL + "/characters/" + characterName + "/card").then((res) => {
       setCharacterInfo(res.data);
+      console.log(res.data);
     });
   }
 }
